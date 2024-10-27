0e679ce8c11e0b9fca7de46c591a371dd63c650b
AO™ <anthony@osawere.com>
2024-10-27 22:15:05 +0100

    Create Gender.php

---
+++ Concept/Enum/Gender.php
@@ -0,0 +1,54 @@
+<?php //*** Gender ~ enum » Yale™ Library - Yet Another Laravel Elevator © 2024 ∞ AO™ • @osawereao • www.osawere.com ∞ Apache License ***//
+
+namespace Yale\Concept\Enum;
+
+enum Gender: string
+{
+	// ◈ case
+	case MALE = 'M';
+	case FEMALE = 'F';
+
+
+
+	// ◈ === toArray »
+	public static function toArray(): array
+	{
+		$gender = [];
+		foreach (self::cases() as $case) {
+			$gender[$case->name] = $case->value;
+		}
+		return $gender;
+	}
+
+
+
+	// ◈ === toObject »
+	public static function toObject(): object
+	{
+		return (object) self::toArray();
+	}
+
+
+
+	// ◈ === label »
+	public function label(): string
+	{
+		// NOTE: How to use > Gender::MALE->label();
+		return match ($this) {
+			self::MALE => 'male',
+			self::FEMALE => 'female',
+		};
+	}
+
+
+
+	// ◈ === number »
+	public function number(): string
+	{
+		return match ($this) {
+			self::MALE => '1',
+			self::FEMALE => '0',
+		};
+	}
+
+}//> end of enum ~ Gender
