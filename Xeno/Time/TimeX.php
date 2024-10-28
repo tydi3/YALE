3dbe3e6e989ef56deb5721af6d3d26b535540ac3
AO™ <anthony@osawere.com>
2024-10-28 18:17:10 +0100

    Update TimeX.php

--- Xeno/Time/TimeX.php
+++ Xeno/Time/TimeX.php
@@ -1,4 +1,4 @@
-<?php //*** TimeX » Yaic™ Library for Laravel © 2024 ∞ AO™ • @osawereao • www.osawere.com ∞ Apache License ***//
+<?php //*** TimeX ~ class » Yale™ Library - Yet Another Laravel Elevator © 2024 ∞ AO™ • @osawereao • www.osawere.com ∞ Apache License ***//
 
 namespace Yale\Xero\Time;
 
