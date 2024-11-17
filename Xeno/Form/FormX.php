1b2bd415864a69a80c882dc8053b69a6ec53c313
AO™ <anthony@osawere.com>
2024-11-17 02:59:01 +0100

    Update FormX.php

--- Xeno/Form/FormX.php
+++ Xeno/Form/FormX.php
@@ -10,4 +10,34 @@ class FormX
 		return new InputX();
 	}
 
+
+
+	// ◈ === format »
+	public static function format(...$argument)
+	{
+		return new FormatX();
+	}
+
+
+
+	// ◈ === set »
+	public static function set(...$argument)
+	{
+		return new SetX();
+	}
+
+
+
+	// ◈ === validateJS »
+	public static function validateJS($id, $btnID = 'sendID', $validateJS)
+	{
+		if ($validateJS === true) {
+			$attr = ' onchange="jsInputValidate({';
+			$attr .= "elemID: '" . $id . "', btnID: '" . $btnID . "'";
+			$attr .= '})"';
+			return $attr;
+		}
+		return '';
+	}
+
 }//> end of class ~ FormX
