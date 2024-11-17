8fc31b8542ccd23e1786731f872fcf3a74710f6d
AO™ <anthony@osawere.com>
2024-11-17 02:58:59 +0100

    Create FormatX.php

---
+++ Xeno/Form/FormatX.php
@@ -0,0 +1,46 @@
+<?php //*** FormatX ~ class » Yale™ Library - Yet Another Laravel Elevator © 2024 ∞ AO™ • @osawereao • www.osawere.com ∞ Apache License ***//
+
+namespace Yale\Xeno\Form;
+
+use Yale\Xeno\Data\StringX;
+
+class FormatX
+{
+	// ◈ === id » format id
+	public static function id($id)
+	{
+		return StringX::swap($id, '.', '_');
+	}
+
+
+
+	// ◈ === label »
+	public static function label($label)
+	{
+		$label = StringX::contain($label, '.') ? StringX::afterLast($label, '.') : $label;
+		$label = StringX::swapSpace($label, '_', true);
+		return StringX::capitalize($label);
+	}
+
+
+
+	// ◈ === title »
+	public static function title($title)
+	{
+		$title = StringX::cropEnd($title, '...');
+		$title = StringX::space($title);
+		if (StringX::countWord($title) == 2) {
+			return ucwords($title);
+		}
+		return StringX::sentenceCase($title);
+	}
+
+
+
+	// ◈ === aria »
+	public static function aria($aria)
+	{
+		return ucfirst($aria);
+	}
+
+}//> end of class ~ FormatX
