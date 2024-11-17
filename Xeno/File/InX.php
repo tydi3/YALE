30d5fa263a40fa3974db5a587445d717798d9e7c
AO™ <anthony@osawere.com>
2024-11-17 05:28:46 +0100

    Create InX.php

---
+++ Xeno/File/InX.php
@@ -0,0 +1,18 @@
+<?php //*** InX ~ FileX » Yale™ Library - Yet Another Laravel Elevator © 2024 ∞ AO™ • @osawereao • www.osawere.com ∞ Apache License ***//
+
+namespace Yale\Xeno;
+
+use Illuminate\Support\Facades\Storage;
+
+class InX
+{
+	// ◈ === storage »
+	public static function storage($file, $public = true)
+	{
+		if ($public) {
+			return Storage::disk('public')->exists($file);
+		}
+		return Storage::exists($file);
+	}
+
+}//> end of class ~ InX
