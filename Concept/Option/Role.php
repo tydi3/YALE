40209e1625be3cbc0deaa253e1186ec6e46ba975
AO™ <anthony@osawere.com>
2024-11-22 17:30:45 +0100

    Role Options

---
+++ Concept/Option/Role.php
@@ -0,0 +1,18 @@
+<?php //*** Role ~ option » Yale™ Library - Yet Another Laravel Elevator © 2024 ∞ AO™ • @osawereao • www.osawere.com ∞ Apache License ***//
+
+namespace Yale\Concept\Option;
+
+use Yale\Concept\Enum\Role as RoleEnum;
+
+class Role
+{
+	// ◈ === option »
+	public static function option($options = []): array
+	{
+		foreach (RoleEnum::cases() as $case) {
+			$options[$case->value] = ucfirst($case->label());
+		}
+		return $options;
+	}
+
+}//> end of option ~ Role
