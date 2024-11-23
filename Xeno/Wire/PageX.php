84ebdf72c8093b53f9a7254e92768b7e054ee0cc
AO™ <anthony@osawere.com>
2024-11-23 21:29:22 +0100

    Update PageX.php

--- Xeno/Wire/PageX.php
+++ Xeno/Wire/PageX.php
@@ -2,11 +2,25 @@
 
 namespace Yale\Xeno\Wire;
 
+use Yale\Xeno\Http\RouteX;
+
 abstract class PageX extends ComponentX
 {
 	// ◈ property
+	protected $routeX;
 	protected $viewX;
 	protected $layoutX;
+
+
+
+	// ◈ === setRouteX »
+	protected function setRouteX(?string $route = null)
+	{
+		if (empty($route)) {
+			$route = RouteX::current('name');
+		}
+		$this->routeX = $route;
+	}
 
 
 
@@ -29,5 +43,49 @@ abstract class PageX extends ComponentX
 			$this->layoutX = $this->iBladeX($layout, $theme);
 		}
 	}
+
+
+
+	// ◈ === getViewX »
+	protected function getViewX(?string $view = null)
+	{
+		if (empty($view)) {
+			if (empty($this->viewX)) {
+				if (!empty($this->moduleX)) {
+					$view = $this->moduleX;
+				} else {
+					$view = strtolower($this->iClassX());
+				}
+				$this->setViewX($view);
+			}
+			return $this->viewX;
+		}
+		return $view;
+	}
+
+
+
+	// ◈ === getLayoutX »
+	protected function getLayoutX(string|bool|null $layout = true)
+	{
+		if (empty($layout) || $layout === true) {
+			if (empty($this->layoutX)) {
+				$this->setLayoutX();
+			}
+			return $this->layoutX;
+		} elseif ($layout === false) {
+			return null;
+		}
+		return $layout;
+	}
+
+
+
+	// ◈ === iPageX »
+	protected function iPageX(?string $view = null, array|object|null $record = null, string|bool|null $layout = true)
+	{
+		return $this->iRenderX($this->getViewX($view), $this->getLayoutX($layout), $record);
+	}
+
 
 }//> end of class ~ PageX
