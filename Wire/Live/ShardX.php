669cc4590ff04fd1c73a0df8d61cf514778b31ff
AO™ <anthony@osawere.com>
2024-12-01 14:44:58 +0100

    Create ShardX.php

---
+++ Wire/Live/ShardX.php
@@ -0,0 +1,45 @@
+<?php //*** ShardX ~ abstract » Yale™ Library - Yet Another Laravel Elevator © 2024 ∞ AO™ • @osawereao • www.osawere.com ∞ Apache License ***//
+
+namespace Yale\Wire\Live;
+
+use Yale\Wire\WireX;
+
+abstract class ShardX extends WireX
+{
+	// ◈ property
+	protected $viewX;
+
+
+
+	// ◈ === setViewX » set view property with blade name
+	protected function setViewX($view, $theme = true, $path = 'shard')
+	{
+		if (!empty($view)) {
+			$this->viewX = $this->formatBladeX($view, $theme, $path);
+		}
+		// TODO: implement an error handler - [view not set]
+	}
+
+
+	// ◈ === cue404X » trigger a 404 error
+	protected function cue404X()
+	{
+	}
+
+
+
+	// ◈ === iShardX »
+	protected function iShardX(?string $view = null, array|object|null $record = null, bool|string|null $theme = true)
+	{
+		if (!empty($view)) {
+			$this->setViewX($view, $theme);
+		}
+		$render = [
+			'view' => $this->getViewX(),
+			'layout' => $this->getLayoutX(),
+			'record' => $record,
+		];
+		return $this->doRenderX($render);
+	}
+
+}//> end of abstract ~ ShardX
