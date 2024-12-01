4a1b311266e51a57a6b8f530bf1f172fd5f95731
AO™ <anthony@osawere.com>
2024-12-01 14:30:18 +0100

    Update WireX.php

--- Wire/WireX.php
+++ Wire/WireX.php
@@ -2,14 +2,15 @@
 
 namespace Yale\Wire;
 
-use Livewire\Component;
 use Yale\Anci\EnvX;
+use Yale\Anci\DebugX;
+use Livewire\Component;
+use Yale\Xeno\File\FileX;
 use Yale\Xeno\Data\StringX;
 
 abstract class WireX extends Component
 {
 	// ◈ property
-	private static $init = false;
 
 
 
@@ -41,6 +42,93 @@ abstract class WireX extends Component
 			return strtolower($blade);
 		}
 		return null;
+	}
+
+
+
+	// ◈ === getViewX »
+	protected function getViewX()
+	{
+		if (!empty($this->viewX)) {
+			return $this->viewX;
+		}
+		return null;
+	}
+
+
+
+	// ◈ === getLayoutX »
+	protected function getLayoutX()
+	{
+		if (!empty($this->layoutX)) {
+			return $this->layoutX;
+		}
+		return null;
+	}
+
+
+
+	// ◈ === isBladeX »
+	protected function isBladeX(?string $blade)
+	{
+		if (!empty($blade) && (FileX::is()->blade($blade))) {
+			return true;
+		}
+		return false;
+	}
+
+
+
+	// ◈ === isViewX » check if view or view set is valid blade
+	protected function isViewX(?string $view = '')
+	{
+		if (!$view || empty($view)) {
+			$view = $this->getViewX();
+		}
+		return $this->isBladeX($view);
+	}
+
+
+
+	// ◈ === isLayoutX » check if layout or layout set is valid blade
+	protected function isLayoutX(?string $layout = '')
+	{
+		if (!$layout || empty($layout)) {
+			$layout = $this->getLayoutX();
+		}
+		return $this->isBladeX($layout);
+	}
+
+
+
+	// ◈ === checkBladeX »
+	protected function checkBladeX(?string $blade, $label = 'bladeX')
+	{
+		if (!$this->isBladeX($blade)) {
+			return DebugX::blade404($blade, $label);
+		}
+	}
+
+
+
+	// ◈ === checkViewX »
+	protected function checkViewX(?string $view = null, $label = 'viewX')
+	{
+		if (empty($view)) {
+			$view = $this->getViewX();
+		}
+		return self::checkBladeX($view, $label);
+	}
+
+
+
+	// ◈ === checkLayoutX »
+	protected function checkLayoutX(?string $layout = null, $label = 'layoutX')
+	{
+		if (empty($layout)) {
+			$layout = $this->getLayoutX();
+		}
+		return self::checkBladeX($layout, $label);
 	}
 
 }//> end of abstract ~ WireX
