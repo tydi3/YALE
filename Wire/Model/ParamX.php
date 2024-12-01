d8acbff9f08100333dd20fda6103f7ea862c16b5
AO™ <anthony@osawere.com>
2024-12-01 22:27:56 +0100

    Update ParamX.php

--- Wire/Model/ParamX.php
+++ Wire/Model/ParamX.php
@@ -7,7 +7,35 @@ use Yale\Xeno\Data\ArrayX;
 trait ParamX
 {
 	// ◈ property
+	protected $idX;
 	protected $recordX;
+
+
+
+	// ◈ === grabIdX »
+	protected function grabIdX($id = null)
+	{
+		if (!empty($id) && empty($this->idX)) {
+			self::setIdX($id);
+		}
+
+		if (!empty($this->idX)) {
+			return $this->idX;
+		}
+
+		// TODO: build an error handler for when $id is expected or record is not found
+		DebugX::oversight($this->moduleX, 'id parameter not found');
+	}
+
+
+
+	// ◈ === setIdX »
+	protected function setIdX(int|string $id = null)
+	{
+		if (!empty($id)) {
+			$this->idX = $id;
+		}
+	}
 
 
 
