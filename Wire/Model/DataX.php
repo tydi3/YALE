3d0a2c8e053f1699d052206c1f1eb337904d34dd
AO™ <anthony@osawere.com>
2024-12-05 19:32:31 +0100

    DataX

---
+++ Wire/Model/DataX.php
@@ -0,0 +1,17 @@
+<?php //*** DataX ~ trait » Yale™ Library - Yet Another Laravel Elevator © 2024 ∞ AO™ • @osawereao • www.osawere.com ∞ Apache License ***//
+
+namespace Yale\Wire\Model;
+
+trait DataX
+{
+	// ◈ === listing »
+	public static function listing(array|string $columns = [], $sortOrder = 'desc', $sortColumn = 'id')
+	{
+		if (empty($columns) && isset(self::$columns)) {
+			$columns = self::$columns;
+		}
+		$result = self::oAll($columns, $sortOrder, $sortColumn);
+		return self::oArray($result);
+	}
+
+}//> end of trait ~ DataX
