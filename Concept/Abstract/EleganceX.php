2f0f8a019760c19abd1f14b11b7c46aec58e697c
AO™ <anthony@osawere.com>
2024-12-05 19:11:35 +0100

    Update EleganceX.php

---
+++ Concept/Abstract/EleganceX.php
@@ -0,0 +1,66 @@
+<?php //*** EleganceX ~ abstract » Yale™ Library - Yet Another Laravel Elevator © 2024 ∞ AO™ • @osawereao • www.osawere.com ∞ Apache License ***//
+
+namespace Yale\Concept\Abstract;
+
+use Illuminate\Database\Eloquent\Model as Eloquent;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\SoftDeletes;
+use Yale\Concept\Trait\Model as ModelX;
+use Yale\Orig\Is;
+
+// use Illuminate\Support\Facades\Log;
+// use Illuminate\Database\QueryException;
+
+abstract class EleganceX extends Eloquent
+{
+	// ◈ traits
+	use HasFactory;
+	use SoftDeletes;
+	use ModelX;
+
+
+
+	// ◈ constants
+	const CREATED_AT = 'created';
+	const UPDATED_AT = 'updated';
+	const DELETED_AT = 'deleted';
+
+
+	// ◈ property
+	protected $dates = ['deleted'];
+
+
+
+	// ◈ === oFilterByID » determine column from id's value
+	protected static function oFilterByID($id, &$filterColumn)
+	{
+		$length = strlen($id);
+		if ($length === 20) {
+			$filterColumn = 'puid';
+		}
+	}
+
+
+
+	// ◈ === oColumn »
+	protected static function oColumn(array|string $columns = ['*'])
+	{
+		$columns = !empty($columns) ? $columns : self::$columns;
+		$columns = is_array($columns) ? $columns : [$columns];
+		return $columns;
+	}
+
+
+
+	// ◈ === oArray »
+	protected static function oArray($result)
+	{
+		if (Is::model($result) || Is::collection($result)) {
+			return $result->toArray();
+		} elseif (empty($result)) {
+			return [];
+		}
+		return $result;
+	}
+
+}//> end of abstract ~ EleganceX
