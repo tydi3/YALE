e4854e8e7b1bcbb851f6c7bf5df16ecab385ecb8
AO™ <anthony@osawere.com>
2024-12-14 14:44:04 +0100

    Update SavingX.php

--- Wire/Render/SavingX.php
+++ Wire/Render/SavingX.php
@@ -11,8 +11,27 @@ trait SavingX
 
 
 
+	// ◈ === iResolveX »
+	protected function iResolveX($action, $response, null|string|array $success = null, ?string $next = null)
+	{
+		if ($response === true || (is_numeric($response) && $response >= 1)) {
+			$this->successX($success);
+			if (!empty($next)) {
+				return $this->redirectX($next);
+			}
+
+			if (in_array($action, ['update'])) {
+				return $this->{$action}($id);
+			} elseif ($action === 'create') {
+				return $this->create();
+			}
+		}
+	}
+
+
+
 	// ◈ === iSavingX »
-	protected function iSavingX($action, $id = null, $input = [], null|string|array $success = null, $next = null)
+	protected function iSavingX($action, $id = null, $input = [], null|string|array $success = null, ?string $next = null)
 	{
 		$this->initX();
 
@@ -21,21 +40,23 @@ trait SavingX
 			$fields = $this->modelX::getFields();
 		}
 
+		$response = false;
 
-		// ~ updating
-		if ($action === 'update' && !empty($id) && !empty($fields)) {
-			$this->grabParamX($input, $fields);
-			$this->cleanInputX($input);
-			$this->factorizeX(input: $input, action: $action);
-			$o = $this->modelX::modify($id, $input);
-			if ($o === true || $o > 0) {
-				$this->successX($success);
-				if ($next) {
-					return $this->redirectX($next);
-				}
-				return $this->update($id);
+		if (in_array($action, ['create', 'update'])) {
+
+			if (!empty($fields)) {
+				$this->grabParamX($input, $fields);
+				$this->cleanInputX($input);
+				$this->factorizeX(input: $input, action: $action);
 			}
+
+			if (!empty($id)) {
+				$response = $this->modelX::modify($id, $input);
+			}
+
 		}
+
+		return $this->iResolveX($action, $response, $success, $next);
 	}
 
 }//> end of trait ~ SavingX
